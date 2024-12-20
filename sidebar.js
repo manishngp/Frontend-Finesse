203e4839e06d98a32765b6b408e31511bdfec91a
manishngp <manishngpdoc@gmail.com>
2024-12-20 14:28:01 +0530

    added

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+document.getElementById("menuButton").addEventListener("click", function () {
+    document.getElementById("sidebar").style.width = "500px";
+  });
+  
+  document.getElementById("closeButton").addEventListener("click", function () {
+    document.getElementById("sidebar").style.width = "0";
+  });
+  
